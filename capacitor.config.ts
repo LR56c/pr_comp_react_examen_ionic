759d906d63bf31c84f36297901c32cacaaf5daa3
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-04-14 16:56:15 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pr_comp_react_examen_ionic',
+  webDir: 'dist'
+};
+
+export default config;

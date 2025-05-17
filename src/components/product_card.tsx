0d46c59970d2bdf20f0b6222d95a020bc0b54d13
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-05-17 12:50:52 -0400

    adjust product card

--- src/components/product_card.tsx
+++ src/components/product_card.tsx
@@ -1,6 +1,6 @@
-import { type  FC }                      from "react"
-import type { Products } from "../modules/domain/products"
-import { Button, Card, Image }  from "react-bootstrap"
+import { type  FC }            from "react"
+import type { Products }       from "../modules/domain/products"
+import { Button, Card, Image } from "react-bootstrap"
 
 interface ProductCardProps {
   product: Products
@@ -12,10 +12,13 @@ export const ProductCard: FC<ProductCardProps> = ( { product, onDelete } ) => {
     <Card
       className="flex flex-col border border-slate-200 rounded-xl p-4 w-full mt-4">
       <div className="flex items-center justify-between">
-        <Image className="object-cover w-24 h-24" src={product.image} rounded />
-        <div>{product.name}</div>
-        <div>{product.price}</div>
-        <div>{product.uploadedBy}</div>
+        <Image className="object-cover w-24 h-24" src={ product.image }
+               rounded/>
+        <div className="flex flex-col justify-between">
+          <div>Nombre: { product.name }</div>
+          <div>Precio: ${ product.price }</div>
+          <div>Subido por: { product.uploadedBy }</div>
+        </div>
         <Button variant="danger" onClick={ () => onDelete( product ) }>
           Eliminar
         </Button>
